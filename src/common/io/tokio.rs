c33eb720ea796f228f4288908eb6296c2c8c209e
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-10-19 19:15:24 +0200

    wip

--- src/common/io/tokio.rs
+++ src/common/io/tokio.rs
@@ -2,13 +2,23 @@
 
 use async_compression::tokio::bufread::GzipDecoder;
 use std::path::Path;
+use std::pin::Pin;
 use tokio::fs::File;
 use tokio::io::{AsyncRead, BufReader};
 
 use crate::common::io::std::is_gz;
 
 /// Transparently open a file with gzip decoder.
-pub async fn open_read_maybe_gz<P>(path: P) -> Result<Box<dyn AsyncRead>, anyhow::Error>
+///
+/// # Arguments
+///
+/// * `path` - A path to the file to open.
+///
+/// # Returns
+///
+/// A `Result` containing a `Pin<Box<dyn AsyncRead>>` that can be used to read
+/// the file, or an `anyhow::Error` if the file could not be opened.
+pub async fn open_read_maybe_gz<P>(path: P) -> Result<Pin<Box<dyn AsyncRead>>, anyhow::Error>
 where
     P: AsRef<Path>,
 {
@@ -28,8 +38,32 @@ where
             decoder.multiple_members(true);
             decoder
         };
-        Ok(Box::new(decoder))
+        Ok(Box::pin(decoder))
     } else {
-        Ok(Box::new(BufReader::new(file)))
+        Ok(Box::pin(BufReader::new(file)))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use tokio::io::AsyncReadExt;
+
+    #[rstest::rstest]
+    #[case("14kb.txt")]
+    #[case("14kb.txt.gz")]
+    #[case("14kb.txt.bgz")]
+    #[tokio::test]
+    async fn open_read_maybe_gz(#[case] path: &str) -> Result<(), anyhow::Error> {
+        mehari::common::set_snapshot_suffix!("{}", path);
+        // Note that the 14kb.txt file contains about 14 KB of data so bgz will have multiple 4KB
+        // blocks.
+
+        let mut reader = super::open_read_maybe_gz(&format!("tests/common/io/{}", path)).await?;
+        let mut buf = Vec::new();
+        reader.read_to_end(&mut buf).await?;
+
+        insta::assert_snapshot!(String::from_utf8(buf)?);
+
+        Ok(())
     }
 }
